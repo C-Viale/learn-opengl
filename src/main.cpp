20fe8d50376bd6d09238c0d6833f1b92e79b75de
Caio Lima <caiovialedl@gmail.com>
2025-08-18 17:59:46 -0300

    Add GLM and remove libraries folder tracking

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include <GLFW/glfw3.h>
 
 #include <algorithm>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <iostream>
 
 #include "shader.h"
